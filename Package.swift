ab504ad424753976097c6f994b7745927ac8989f
Anton Remizov <anton@calthings.com>
2019-11-12 22:00:06 -0800

    removed unused dependencies

--- Package.swift
+++ Package.swift
@@ -1,12 +1,13 @@
-// Generated automatically by Perfect Assistant Application
-// Date: 2017-05-09 18:49:29 +0000
+// swift-tools-version:5.0
 import PackageDescription
+
 let package = Package(
 	name: "PerfectStripe",
-	targets: [],
 	dependencies: [
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-Logger.git", majorVersion: 3),
-		.Package(url: "https://github.com/iamjono/SwiftString.git", majorVersion: 2),
-		.Package(url: "https://github.com/iamjono/codableRequest.git", majorVersion: 1),
-	]
+		.package(url: "https://github.com/iamjono/codableRequest.git", from: "1.0.0"),
+	],
+    targets: [
+    .target(name: "PerfectStripe"),
+    ],
+    swiftLanguageVersions: [.v5]
 )
